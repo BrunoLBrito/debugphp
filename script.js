5449ad8f04a04d93215d935da57323286d21fee8
Bruno Brito <bruno-lucas@msn.com>
2020-01-12 04:42:03 -0300

    add loading, optimization fetch async

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let form = document.querySelector('form')
 let demo = document.querySelector('.demo')
 
-form.onsubmit = e => {
+form.onsubmit = async e => {
 	e.preventDefault()
+
+	demo.innerHTML = '<span>Loading...</span>'
 
 	let data = new FormData(form)
 
@@ -12,14 +14,27 @@ form.onsubmit = e => {
 		//mode: 'no-cors'
 	}
 
-	fetch('https://granpharmaceuticals.herokuapp.com/', options)
-		.then(res => res.json())
-		.then(result => {
-			let text = `
-            <p>${result.name}</p>
-            <p>${result.email}</p>
-         `
-			demo.innerHTML = text
-		})
-		.catch(err => console.error(err))
+	// fetch('https://granpharmaceuticals.herokuapp.com/', options)
+	// 	.then(res => res.json())
+	// 	.then(result => {
+	// 		let text = `
+	//          <p>${result.name}</p>
+	//          <p>${result.email}</p>
+	//       `
+	// 		demo.innerHTML = text
+	// 	})
+	// 	.catch(err => console.error(err))
+
+	try {
+		let response = await fetch('https://granpharmaceuticals.herokuapp.com/', options)
+		let result = await response.json()
+
+		let text = `
+		      <p>${result.name}</p>
+		      <p>${result.email}</p>
+		   `
+		demo.innerHTML = text
+	} catch (err) {
+		demo.innerHTML = `Error: ${err.message}`
+	}
 }
--- send.php
+++ send.php
@@ -1,9 +1,12 @@
 <?php
 
+sleep(4);
 
+if(isset($_POST)){
    $data = array(
       "name" => $_POST['name'],
       "email" => $_POST['email'],
    );
+}
 
 echo json_encode($data);
